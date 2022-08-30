ba2346e4b255f1b2c370cfb1df3c9e0f8bfcbf29
Julien Thierry <julienthierrypro@gmail.com>
2022-08-30 14:19:37 +0200

    countdown js

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+let totalSeconds, minutes, seconds, interval;
 
-choice.value = 5 minutes
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  totalSeconds = choice.value * 60;
+  minutes = Math.floor(totalSeconds / 60);
+  seconds = totalSeconds % 60;
+  clearInterval(interval);
+  interval = setInterval(() => {
+    countDown();
+  }, 100);
+  countdownDisplay.textContent = minutes + ":" + seconds + "0";
+  choice.value = "";
+});
 
-choice.value * 60 = totalSeconds
+const countDown = () => {
+  if (minutes === 0 && seconds === 0) {
+    clearInterval(interval);
+    countdownDisplay.textContent = "compte a rebours terminé ! ";
+    return;
+  } else if (seconds === 0) {
+    minutes--;
+    seconds = 59;
+  } else {
+    seconds--;
+  }
 
-let minutes = Math.floor(totalSeconds / 60);
-let seconds = totalSeconds % 60
-
-
+  countdownDisplay.textContent = minutes + ":" + seconds;
+};
